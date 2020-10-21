9f06bcc62a6f136e8d65a3a8cfe2611455bb0e3b
Moringa School Access <wambuimbeatrice@gmail.com>
2020-10-21 13:42:23 +0300

    generate the getters and setters of the attributes

--- src/main/java/modules/Squad.java
+++ src/main/java/modules/Squad.java
@@ -7,4 +7,36 @@ public class Squad {
     private String squadGender;
     private int squadId;
     /* the property attributes have to be private to avoid being accessed directly without getters and setters or modified, thus, reducing the pesky bugs. */
+
+    public String getSquadGender() {
+        return squadGender;
+    }
+
+    public void setSquadGender(String squadGender) {
+        this.squadGender = squadGender;
+    }
+
+    public String getSquadName() {
+        return squadName;
+    }
+
+    public void setSquadName(String squadName) {
+        this.squadName = squadName;
+    }
+
+    public int getSquadNumber() {
+        return squadNumber;
+    }
+
+    public void setSquadNumber(int squadNumber) {
+        this.squadNumber = squadNumber;
+    }
+
+    public String getSquadPurpose() {
+        return squadPurpose;
+    }
+
+    public void setSquadPurpose(String squadPurpose) {
+        this.squadPurpose = squadPurpose;
+    }
 }

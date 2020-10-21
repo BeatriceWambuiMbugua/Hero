0a2befff3974c92c3dc6ff2aaeee006ce8cc2590
Moringa School Access <wambuimbeatrice@gmail.com>
2020-10-21 13:40:12 +0300

    created the Squad.java property attributes

---
+++ src/main/java/modules/Squad.java
@@ -0,0 +1,10 @@
+package modules;
+
+public class Squad {
+    private String squadName;
+    private String squadPurpose;
+    private int squadNumber;
+    private String squadGender;
+    private int squadId;
+    /* the property attributes have to be private to avoid being accessed directly without getters and setters or modified, thus, reducing the pesky bugs. */
+}

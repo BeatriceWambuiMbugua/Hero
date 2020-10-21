2859d6e5d948002da7e7a2af1d22d0d7363bba17
Moringa School Access <wambuimbeatrice@gmail.com>
2020-10-21 18:58:14 +0300

    created an interface method

--- src/main/java/DAO/Sql2oSquadDAO.java
+++ src/main/java/DAO/Sql2oSquadDAO.java
@@ -1,4 +1,18 @@
 package DAO;
 
+import modules.Squad;
+import org.sql2o.Sql2o;
+
+import java.util.List;
+
 public class Sql2oSquadDAO  implements  SquadDAO{
+    private final Sql2o sql2o;
+
+    public Sql2oSquadDAO(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+    @Override
+    public List<Squad> getAllSquads() {
+        return null;
+    }
 }
--- src/main/java/DAO/SquadDAO.java
+++ src/main/java/DAO/SquadDAO.java
@@ -1,4 +1,10 @@
 package DAO;
 
+import modules.Squad;
+
+import java.util.List;
+
 public interface SquadDAO {
+    //List
+    List<Squad> getAllSquads();
 }

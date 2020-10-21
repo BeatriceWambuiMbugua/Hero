0b8f4e9b012ca403c03754077e3432281b4216c4
Moringa School Access <wambuimbeatrice@gmail.com>
2020-10-21 19:45:06 +0300

    generated the error handling methods

--- src/main/java/DAO/Sql2oSquadDAO.java
+++ src/main/java/DAO/Sql2oSquadDAO.java
@@ -1,18 +1,30 @@
 package DAO;
 
 import modules.Squad;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 import java.util.List;
 
 public class Sql2oSquadDAO  implements  SquadDAO{
+    /* create Sql2o object to enable the Sql2oSquadDAO to fetch data from the database */
     private final Sql2o sql2o;
 
     public Sql2oSquadDAO(Sql2o sql2o) {
         this.sql2o = sql2o;
     }
+    //Override the method to implement the interface
     @Override
     public List<Squad> getAllSquads() {
-        return null;
+        String sql = "SELECT * FROM squads";
+        //error handling using try and catch
+        try(Connection con = sql2o.open()){
+            return con.createQuery(sql)
+                    .executeAndFetch(Squad.class);
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+            return null;
+        }
     }
 }

ed43a823ce867e35993e8a674beb05246b80b0e3
Moringa School Access <wambuimbeatrice@gmail.com>
2020-10-22 21:06:48 +0300

    created hero-form.hbs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 import static spark.Spark.*;
 
+import DAO.Sql2oHeroDAO;
 import DAO.Sql2oSquadDAO;
 import org.sql2o.Connection;
 import modules.Squad;
@@ -8,6 +9,7 @@ import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,11 +21,15 @@ public class App {
         Connection con;
         Sql2o sql2o = new Sql2o(connectionString, "", "");
         Sql2oSquadDAO squadDAO = new Sql2oSquadDAO(sql2o);
+        Sql2oHeroDAO heroDAO = new Sql2oHeroDAO(sql2o);
         Map<String, Object> model = new HashMap<>();
         //Declare the Map<> globally to DRY the code
 
+        List<String> myStrings = new ArrayList<>();
+
         get("/", (req, res) -> {
             model.put("squads", squadDAO.getAllSquads());
+            model.put("heroes", heroDAO.getAllHeroes());
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
